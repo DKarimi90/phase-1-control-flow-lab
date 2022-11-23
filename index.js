b3fae5b5fe2583ac51a02c2028f5a67c90875904
david <davmwa90@gmail.com>
2022-11-23 12:59:40 +0300

    Used if, else if, ternary, and switch statements

--- index.js
+++ index.js
@@ -26,9 +26,11 @@ return city === 'NYC' ? "Ok, sounds good." : "No go."
 function switchOnCharmFromTip(tip){  
   switch(tip) {
     case 'generous': 
-          return "Thank you so much."; 
+          return "Thank you so much.";
+          break;  
     case 'not as generous': 
           return "Thank you."; 
+          break; 
     default: 
           return "Bye."; 
   }
